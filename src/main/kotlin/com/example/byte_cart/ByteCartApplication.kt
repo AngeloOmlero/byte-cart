3bccc59b3fd2d131753993eedc7ea2beb046a750
Angelo Omlero <aomlero@vauldex.com>
2025-12-05 08:35:00 +0800

    Initial commit

---
+++ src/main/kotlin/com/example/byte_cart/ByteCartApplication.kt
@@ -0,0 +1,11 @@
+package com.example.byte_cart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ByteCartApplication
+
+fun main(args: Array<String>) {
+	runApplication<ByteCartApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/byte_cart/ByteCartApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.byte_cart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ByteCartApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
